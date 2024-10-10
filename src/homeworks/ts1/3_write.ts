19dc9fcbdf0001f25b354a513cf429c95d51a5da
ches <ches@nordciti.ru>
2024-10-10 09:30:48 +0300

    add: homework1

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -43,14 +43,102 @@
  * - type ('Profit')
  * */
 
-/**
- * Создает случайный продукт (Product).
- * Принимает дату создания (строка)
- * */
-// export const createRandomProduct = (createdAt: string) => {};
+export type Category = {
+    id: string;
+    name: string;
+    photo?: string;
+}
 
-/**
- * Создает случайную операцию (Operation).
- * Принимает дату создания (строка)
- * */
-// export const createRandomOperation = (createdAt: string) => {};
+export type Product = {
+    id: string;
+    name: string;
+    photo?: string;
+    desc?: string;
+    createdAt: string;
+    oldPrice?: number;
+    price: number;
+    category: Category;
+}
+
+export type Operation = Cost | Profit;
+
+
+export type Cost = {
+    id: string;
+    name: string;
+    desc?: string;
+    createdAt: string;
+    amount: number;
+    category: Category;
+    type: 'Cost';
+}
+
+export type Profit = {
+    id: string;
+    name: string;
+    desc?: string;
+    createdAt: string;
+    amount: number;
+    category: Category;
+    type: 'Profit';
+}
+
+
+export const createRandomProduct = (createdAt: string): Product => {
+    const category = createRandomCategory();
+  
+    const productId: number = Math.floor(Math.random() * 1000);
+
+    const product: Product = {
+      id: productId.toString(),
+      name: `Продукт №${productId}`,
+      photo: `Фото Продукта №${productId}`,
+      desc: `Описание Продукта №${productId}`,
+      createdAt: createdAt,
+      oldPrice: Math.random() * 1000,
+      price: Math.random() * 1000,
+      category: category
+    };
+
+    return product;
+  };
+  
+  /**
+   * Создает случайную операцию (Operation).
+   * Принимает дату создания (строка)
+   * */
+  export const createRandomOperation = (createdAt: string): Operation => {
+    const category = createRandomCategory();
+  
+    const operationId: number = Math.floor(Math.random() * 1000);
+
+    const operationType: 'Profit' | 'Cost' = Math.random() > 0.5 ? 'Profit' : 'Cost';
+
+    const operation: Operation = {
+      id: operationId.toString(),
+      name: `Операция №${operationId}`,
+      desc: `Описание Операции ${operationType} №${operationId}`,
+      createdAt: createdAt,
+      amount: Math.floor(Math.random() * 100),
+      category: category,
+      type: operationType
+    };
+
+    return operation;
+  };
+  
+  /**
+   * Создает случайную категорию (Category).
+   * Без входных параметров
+   * */
+  const createRandomCategory = (): Category => {
+    const categoryId: number = Math.floor(Math.random() * 1000);
+
+    const category: Category = {
+      id: categoryId.toString(),
+      name: `Категория №${categoryId}`,
+      photo: `Фото для Категории №${categoryId}`
+    };
+    
+    return category;
+  }
